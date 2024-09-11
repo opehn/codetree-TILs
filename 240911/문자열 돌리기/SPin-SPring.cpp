f2581b735f71c28afe9b905d81894173b22b7c83
opehn <67584727+opehn@users.noreply.github.com>
2024-09-11 13:00:33 +0900

    Title: 문자열 돌리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240911/문자열 돌리기/SPin-SPring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    for (int i = 0; i < str.length() + 1; i++) {
+        cout << str << endl;
+        str = str.substr(str.length() - 1, 1) + str.substr(0, str.length() - 1); 
+    }
+    return 0;
+}
