fa137d5eac5a809c88b1ab916383e17e8b09f8a8
opehn <67584727+opehn@users.noreply.github.com>
2024-09-11 14:21:16 +0900

    Title: 정수만 추출하기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240911/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+
+    cin >> a >> b;
+
+    int aIdx = 0;
+    for(int i = 0; i < a.length(); i++){
+        if (a[i] < '0' && a[i] > '9')
+            break;
+        aIdx++;
+    }
+    
+    int bIdx = 0;
+    for(int i = 0; i < a.length(); i++){
+        if (a[i] < '0' && a[i] > '9')
+            break;
+        bIdx++;
+    }
+    
+    int res1 = stoi(a.substr(0, aIdx));
+    int res2 = stoi(b.substr(0, bIdx));
+
+    cout << res1 + res2 << endl;
+    return 0;
+}
