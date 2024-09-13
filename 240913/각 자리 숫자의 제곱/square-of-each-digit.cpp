b69b293e93000b94f71443aadf2f910ef2cedc11
opehn <67584727+opehn@users.noreply.github.com>
2024-09-13 11:11:13 +0900

    Title: 각 자리 숫자의 제곱, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/각 자리 숫자의 제곱/square-of-each-digit.cpp
+++ 240913/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int F(int n) {
     if (n < 10) 
-        return n;
-    return F(n - 1) * n;
+        return n * n;
+    return F(n / 10) + (n % 10) * (n % 10);
     
 }
 
 int main() {
+    int n = 0;
+
+    cin >> n;
+
     cout << F(n);
     return 0;
 }
