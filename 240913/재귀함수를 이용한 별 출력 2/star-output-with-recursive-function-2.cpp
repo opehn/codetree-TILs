b01ad937a92dd4e3e62cd144f7224ed01b1a4639
opehn <67584727+opehn@users.noreply.github.com>
2024-09-13 10:21:51 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void printStar(int n){
+    if (n == 0) 
+        return;
+
+    for (int i = 0; i < n; i++) {
+        cout << "*" << " ";
+    }
+    cout << endl;
+
+    printStar(n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << "*" << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 0;
+    
+    cin >> n;
+    printStar(n);
+    return 0;
+}
