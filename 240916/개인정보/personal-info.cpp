d8cc5f76192f3b3ab74bb4047fb17759dc7a948d
opehn <67584727+opehn@users.noreply.github.com>
2024-09-16 13:06:09 +0900

    Title: 개인정보, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/개인정보/personal-info.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+#define MAX_PERSON 5
+
+bool cmpByName(tuple<string, int, double>a, tuple<string, int, double>b){
+    string name1, name2;
+
+    tie(name1, ignore, ignore) = a;
+    tie(name2, ignore, ignore) = b;
+
+    return name1 < name2;
+}
+
+bool cmpByHeight(tuple<string, int, double>a, tuple<string, int, double>b){
+    int height1, height2;
+
+    tie(ignore, height1, ignore) = a;
+    tie(ignore, height2, ignore) = b;
+
+    return height1 > height2;
+}
+
+int main() {
+    tuple<string, int, double> persons[MAX_PERSON];
+
+    string name;
+    int height;
+    double weight;
+    for(int i = 0; i < MAX_PERSON; i++){
+        cin >> name >> height >> weight;
+        persons[i] = make_tuple(name, height, weight);
+    }
+
+    sort(persons, persons + MAX_PERSON, cmpByName);
+
+    cout << "name" << endl;
+    for(int i = 0; i < MAX_PERSON; i++){
+        tie(name, height, weight) = persons[i];
+        cout << name << " " << height << " " << weight << endl;
+    }
+
+    sort(persons, persons + MAX_PERSON, cmpByHeight);
+    cout << endl << "height" << endl;
+    for(int i = 0; i < MAX_PERSON; i++){
+        tie(name, height, weight) = persons[i];
+        cout << name << " " << height << " " << weight << endl;
+    }
+    return 0;
+}
