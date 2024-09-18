8e20f97b1fafd91a7333a3f5b60b512ddfa08aaa
opehn <67584727+opehn@users.noreply.github.com>
2024-09-18 14:36:25 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240918/Date to Date/date-to-date.cpp
+++ 240918/Date to Date/date-to-date.cpp
@@ -17,10 +17,10 @@ int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int dayPassed1 = getDayPassed(m1, d1);
+    int dayPassed1 = getDayPassed(m1, d1) + 1;
     int dayPassed2 = getDayPassed(m2, d2);
 
-    
-    cout << (dayPassed1 == dayPassed2 ? 1 : dayPassed2 - dayPassed1) << endl;
+
+    cout << dayPassed2 - dayPassed1 + 1 << endl;
     return 0;
 }
