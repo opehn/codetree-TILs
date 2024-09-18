e1cd656585389f7b9d8f97beaf15d2bb7909fb11
opehn <67584727+opehn@users.noreply.github.com>
2024-09-18 14:31:13 +0900

    Title: Date to Date, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240918/Date to Date/date-to-date.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int getDayPassed(int month, int day)  {
+    int numOfDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int dayPassed = 0;
+
+    for(int i = 1;i <= month; i++){
+        dayPassed += numOfDays[i];
+    }
+    dayPassed += day;
+
+    return dayPassed;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int dayPassed1 = getDayPassed(m1, d1);
+    int dayPassed2 = getDayPassed(m2, d2);
+
+    cout << dayPassed2 - dayPassed1 << endl;
+    return 0;
+}
