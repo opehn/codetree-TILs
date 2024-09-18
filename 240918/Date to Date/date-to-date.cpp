8c5ff970385a69dfec647b85a1bb9dffc270d6ab
opehn <67584727+opehn@users.noreply.github.com>
2024-09-18 15:10:26 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240918/Date to Date/date-to-date.cpp
+++ 240918/Date to Date/date-to-date.cpp
@@ -5,7 +5,7 @@ int getDayPassed(int month, int day)  {
     int numOfDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int dayPassed = 0;
 
-    for(int i = 1;i <= month; i++){
+    for(int i = 1;i < month; i++){
         dayPassed += numOfDays[i];
     }
     dayPassed += day;
@@ -17,9 +17,8 @@ int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int dayPassed1 = getDayPassed(m1, d1) + 1;
+    int dayPassed1 = getDayPassed(m1, d1);
     int dayPassed2 = getDayPassed(m2, d2);
-
 
     cout << dayPassed2 - dayPassed1 + 1 << endl;
     return 0;
