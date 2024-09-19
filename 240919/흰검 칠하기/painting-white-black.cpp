1ecfe900fbc3d816a2fe23a9b8c269b0db9c6573
opehn <67584727+opehn@users.noreply.github.com>
2024-09-19 14:30:53 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 2MB, Status: Passed - CodeTree

--- 240919/흰검 칠하기/painting-white-black.cpp
+++ 240919/흰검 칠하기/painting-white-black.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
 
-// #define MAX_N 1000
-// #define MAX_R 200000
-// #define OFFSET 100000
-#define MAX_N 20
-#define MAX_R 10
-#define OFFSET 5
+#define MAX_N 1000
+#define MAX_R 200000
+#define OFFSET 100000
 #define BLACK -1
 #define WHITE 1
+#define GRAY 2
 
 int main() {
     int n;
@@ -30,17 +28,25 @@ int main() {
         if (direction == "R"){
             end = start + distance;
             for(int i = start; i < end; i++){
-                line[i] = BLACK;
                 black[i]++;
+                if (black[i] >=2 && white[i] >= 2){
+                    line[i] = GRAY;
+                } else {
+                    line[i] = BLACK;
+                }
             }
-            cur = end;
+            cur = end - 1;
         } else {
             end = start - distance;
-            for(int i = start - 1; i >= end; i--){
-                line[i] = WHITE;
+            for(int i = start; i > end; i--){
                 white[i]++;
+                if (black[i] >=2 && white[i] >= 2){
+                    line[i] = GRAY;
+                } else {
+                    line[i] = WHITE;
+                }
             }
-            cur = end;
+            cur = end + 1;
         }
     }
 
@@ -49,12 +55,9 @@ int main() {
     int whiteCnt = 0;
     int grayCnt = 0;
     for(int i = 0; i < MAX_R + 1; i++){
-        if (black[i] >=2 && white[i] >= 2){
-            grayCnt++;
-            continue;
-        }
-        if(line[i] == BLACK) blackCnt++;
-        if(line[i] == WHITE) whiteCnt++;
+        if(line[i] == GRAY) grayCnt++;
+        else if(line[i] == BLACK) blackCnt++;
+        else if(line[i] == WHITE) whiteCnt++;
     }
 
     cout << whiteCnt << " " << blackCnt << " " << grayCnt << endl;
