6a63791ea25192eefa601d6f86cba7af90041ed4
opehn <67584727+opehn@users.noreply.github.com>
2024-09-21 10:35:35 +0900

    Title: 벌금은 누구에게, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    int students[n + 1] = {};
+    int timeline[m] = {};
+
+    int target;
+    for(int sec = 0; sec < m; sec++){
+        cin >> target;
+        students[target]++;
+        if (students[target] >= k) {
+            timeline[sec] = target;
+        }
+    }
+
+    int ans = -1;
+    for(int sec = 0; sec < m; sec++){
+        if (timeline[sec]) {
+            ans = timeline[sec];
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
