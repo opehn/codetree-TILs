2eb84d8a756761262d4781c039dbe41bce1cd692
opehn <67584727+opehn@users.noreply.github.com>
2024-09-21 13:39:51 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+#define MAX_TIME 250
+int main() {
+    int n, k, p, t;
+    cin >> n >> k >> p >> t;
+
+    int actualTime = 0;
+    int persons[n + 1] = {};
+    int count[n + 1] = {};
+    persons[p] = 1;
+    pair<int, int> handshakes[MAX_TIME];
+    fill(handshakes, handshakes + MAX_TIME, make_pair(-1, -1));
+    int s, x, y;
+    for(int i = 0; i < t; i++) {
+        cin >> s >> x >> y;
+        handshakes[s - 1] = make_pair(x, y);
+        actualTime = actualTime > s ? actualTime : s;
+    }
+
+    for(int i = 0; i < actualTime; i++){
+        if(handshakes[i].first != -1 && count[handshakes[i].first] < k) {
+            persons[handshakes[i].second] = 1;
+        }
+        count[handshakes[i].first]++;
+    }  
+        
+    for(int i = 1; i <= n; i++){
+        cout << persons[i];
+    }
+    return 0;
+}
