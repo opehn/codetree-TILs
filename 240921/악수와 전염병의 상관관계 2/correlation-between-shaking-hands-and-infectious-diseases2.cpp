070121fc507be21ae7050168a2e92f9b05ce519f
opehn <67584727+opehn@users.noreply.github.com>
2024-09-21 13:58:54 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240921/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240921/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <utility>
+#include <tuple>
+#include <algorithm>
 using namespace std;
 
 #define MAX_TIME 250
@@ -7,31 +8,29 @@ int main() {
     int n, k, p, t;
     cin >> n >> k >> p >> t;
 
-    int actualTime = 0;
     int persons[n + 1] = {};
     int count[n + 1] = {};
     persons[p] = 1;
-    pair<int, int> handshakes[MAX_TIME];
-    fill(handshakes, handshakes + MAX_TIME, make_pair(-1, -1));
+    tuple<int, int, int> handshakes[t];
     int s, x, y;
     for(int i = 0; i < t; i++) {
         cin >> s >> x >> y;
-        handshakes[s - 1] = make_pair(x, y);
-        actualTime = actualTime > s ? actualTime : s;
+        handshakes[i] = make_tuple(s, x, y);
     }
+    sort(handshakes, handshakes + t);
 
-    for(int i = 0; i < actualTime; i++){
-        if(handshakes[i].first != -1 && persons[handshakes[i].first] == 1 
-        && count[handshakes[i].first] < k) {
-            persons[handshakes[i].second] = 1;
+    for(int i = 0; i < t; i++){
+        tie(s, x, y) = handshakes[i];        
+        if(persons[x] == 1 
+        && count[x] < k) {
+            persons[y] = 1;
         }
-        if(handshakes[i].second != -1 && persons[handshakes[i].second] == 1
-        && count[handshakes[i].second] < k) {
-            persons[handshakes[i].first] = 1;
+        if(persons[y] == 1 && count[y] < k) {
+            persons[x] = 1;
         }
         
-        count[handshakes[i].first]++;
-        count[handshakes[i].second]++;
+        count[x]++;
+        count[y]++;
     }  
         
     for(int i = 1; i <= n; i++){
