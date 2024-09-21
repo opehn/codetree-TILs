5f31731786c0be07cd85d8c10b91bb129d5bf1aa
opehn <67584727+opehn@users.noreply.github.com>
2024-09-21 10:12:00 +0900

    Title: 만나는 그 순간, Time: 20ms, Memory: 7MB, Status: Passed - CodeTree

--- 240921/만나는 그 순간/the-moment-we-meet.cpp
+++ 240921/만나는 그 순간/the-moment-we-meet.cpp
@@ -6,8 +6,10 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int a[MAX_LENGTH] = {}; 
-    int b[MAX_LENGTH] = {};
+    int a[MAX_LENGTH]; 
+    int b[MAX_LENGTH];
+    fill(a, a + MAX_LENGTH, MAX_LENGTH + 1);
+    fill(b, b + MAX_LENGTH, MAX_LENGTH + 1);
 
     string direction;
     int second;
@@ -42,12 +44,17 @@ int main() {
         }
     }
 
+    int flag = 0;
     for(int i = 0; i < MAX_LENGTH; i++){
-        if(a[i] == b[i]) {
+        if(a[i] != MAX_LENGTH + 1 && b[i] != MAX_LENGTH + 1 &&
+        a[i] == b[i]) {
+            flag = 1;
             cout << i + 1 << endl;
             break;
         }
     }
 
+    if(!flag)
+        cout << -1 << endl;
     return 0;
 }
