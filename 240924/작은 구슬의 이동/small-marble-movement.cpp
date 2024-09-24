893aec5a610ee1b272652e4c46221810a5889da8
opehn <67584727+opehn@users.noreply.github.com>
2024-09-24 12:37:29 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240924/작은 구슬의 이동/small-marble-movement.cpp
+++ 240924/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int getDir(string dir) {
-    if(dir == "U") 
-        return 0;
-    if(dir == "D") 
-        return 3;
+int getdir(string dir) {
     if(dir == "R") 
-        return 2;
+        return 0;
     if(dir == "L") 
+        return 3;
+    if(dir == "U") 
+        return 2;
+    if(dir == "D") 
         return 1;
 
     return -1;
 }
 
 int inRange(int x, int y, int n) {
-    return (x >= 0 && x < n && y >= 0 && y < n);
+    return (x >= 1 && x <= n && y >= 1 && y <= n);
 }
 
 int main() {
-    int n, t, x, y;
+    int n, t, r, c;
     string dir;
 
-    cin >> n >> t >> x >> y >> dir;
+    cin >> n >> t >> r >> c >> dir;
 
-    int dx[4] = {0, 1, -1, 0}, dy[4] = {1, 0, 0, -1};
+    int dr[4] = {0, -1, 1, 0}, dc[4] = {1, 0, 0, -1};
     
-    int dirNum = getDir(dir);
+    int dirNum = getdir(dir);
 
 
-    int cnt = 0;
+    int nr, nc;
     for(int i = 0; i < t; i++){
-        x += dx[dirNum];
-        y += dy[dirNum];
-        if (!inRange(x, y, n))
+        nr = r + dr[dirNum], nc = c + dc[dirNum];
+
+        if (!inRange(nr, nc, n)){
             dirNum = 3 - dirNum; 
+        } else {
+            r += dr[dirNum];
+            c += dc[dirNum];
+        }
     }
-    cout << x << " " <<  y << endl;
+    cout << r << " " <<  c << endl;
 
     return 0;
 }
