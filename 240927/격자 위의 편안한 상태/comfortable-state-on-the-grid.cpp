c2b2fd47b5bf4711d69132c18fd57b00d47effb2
opehn <67584727+opehn@users.noreply.github.com>
2024-09-27 12:40:15 +0900

    Title: 격자 위의 편안한 상태, Time: 21ms, Memory: 0MB, Status: Passed - CodeTree

--- 240927/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 240927/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -4,20 +4,20 @@ using namespace std;
 bool inRange(int r, int c, int n){
     return (r >= 0 && r < n && c >= 0 && c < n);
 }
-
 int main() {
     int n, m;
     cin >> n >> m;
 
-    int grid[100][100] = {0};  // 정적 할당으로 변경
+    int grid[n][n] = {};
     int r, c;
-    int aroundR, aroundC;
+    int aroundR = 0, aroundC = 0;
     int dr[4] = {0, 1, 0, -1};
     int dc[4] = {1, 0, -1, 0};
     int cnt;
     for(int i = 0; i < m; i++){
         cin >> r >> c;
-        r--; c--;
+        r--;
+        c--;
         grid[r][c] = 1;
         cnt = 0;
         for(int j = 0; j < 4; j++){
