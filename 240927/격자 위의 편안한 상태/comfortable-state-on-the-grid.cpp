af56c66038cd42c1dca71c7747c06e1098319026
opehn <67584727+opehn@users.noreply.github.com>
2024-09-27 12:18:11 +0900

    Title: 격자 위의 편안한 상태, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 240927/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 240927/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -4,33 +4,29 @@ using namespace std;
 bool inRange(int r, int c, int n){
     return (r >= 0 && r < n && c >= 0 && c < n);
 }
+
 int main() {
     int n, m;
     cin >> n >> m;
 
-    int grid[n][n] = {};
+    int grid[100][100] = {0};  // 정적 할당으로 변경
     int r, c;
-    int aroundR = 0, aroundC = 0;
+    int aroundR, aroundC;
     int dr[4] = {0, 1, 0, -1};
     int dc[4] = {1, 0, -1, 0};
-    int cnt = 0;
+    int cnt;
     for(int i = 0; i < m; i++){
         cin >> r >> c;
-        r--;
-        c--;
+        r--; c--;
         grid[r][c] = 1;
+        cnt = 0;
         for(int j = 0; j < 4; j++){
-            aroundR = r + dr[j], aroundC = c + dc[j];
-            if(inRange(r, c, n) && grid[aroundR][aroundC] == 1)
+            aroundR = r + dr[j];
+            aroundC = c + dc[j];
+            if(inRange(aroundR, aroundC, n) && grid[aroundR][aroundC] == 1)
                 cnt++;
         }
-        if(cnt == 3) {
-            cout << 1 << endl;
-        } else {
-            cout << 0 << endl;
-        }
-        cnt = 0;
+        cout << (cnt == 3) << endl;
     }
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
