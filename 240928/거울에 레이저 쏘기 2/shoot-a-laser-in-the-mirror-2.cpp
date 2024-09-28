f01bb31b2b4ba340c670c13ecd4db6b45a2384b0
opehn <67584727+opehn@users.noreply.github.com>
2024-09-28 12:36:53 +0900

    Title: 거울에 레이저 쏘기 2, Time: 33ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240928/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240928/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -3,16 +3,15 @@ using namespace std;
 
 int r, c, dir;
 void getFirstPosition(int k, int n){
-    int r, c, dir;
     if(k <= n) {
         dir = 2;
         r = 0;
         c = k - 1;
-    } else if(num <= 2* n) {
+    } else if(k <= 2* n) {
         dir = 3;
         r = k - n - 1;
         c = n - 1; 
-    } else if(num <= 3 * n) {
+    } else if(k <= 3 * n) {
         dir = 0;
         r = n - 1;
         c = n - (k - (n * 2));
@@ -23,25 +22,43 @@ void getFirstPosition(int k, int n){
     }
 }
 
+bool inRange(int r, int c, int n) {
+    return (r >= 0 && r < n && c >= 0 && c < n);
+}
+
 int main() {
     int n, k;
     cin >> n;
 
     int dr[4] = { 0, 1, 0, -1};
     int dc[4] = { -1, 0, 1, 0};
-    string slash[4] = {1, -1, 1, -1};
-    string backSlash[4] = {-1, 1, -1, 1};
+    int slash[4] = {1, -1, 1, -1};
+    int backSlash[4] = {-1, 1, -1, 1};
 
-    string grid[n][n] = {};
-    string slash;
+    char grid[n][n] = {};
+    string slashes;
     for(int i = 0; i < n; i++) {
+        cin >> slashes;
         for(int j = 0; j < n; j++){
-            cin >> grid[i][j];
+            grid[i][j] = slashes[j];
         }
     }
 
     cin >> k;
     getFirstPosition(k, n);
-    cout << dir << " " << r << " " << c << endl;
+
+    int cnt = 0;    
+    while(inRange(r, c, n)) {
+        r += dr[dir];
+        c += dc[dir];
+        if (grid[r][c] == '\\') {
+            dir += backSlash[dir];
+        } else {
+            dir += slash[dir];
+        }
+        cnt++;
+    }
+
+    cout << cnt << endl;
     return 0;
 }
