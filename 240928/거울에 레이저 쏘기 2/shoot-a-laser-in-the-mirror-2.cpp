8b33e22409a25ce44f1db37be51622cbc78f3721
opehn <67584727+opehn@users.noreply.github.com>
2024-09-28 12:45:04 +0900

    Title: 거울에 레이저 쏘기 2, Time: 23ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240928/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240928/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -49,14 +49,15 @@ int main() {
 
     int cnt = 0;    
     while(inRange(r, c, n)) {
+        cnt++;
         r += dr[dir];
         c += dc[dir];
+        if (!inRange(r, c, n)) break;
         if (grid[r][c] == '\\') {
             dir += backSlash[dir];
         } else {
             dir += slash[dir];
         }
-        cnt++;
     }
 
     cout << cnt << endl;
