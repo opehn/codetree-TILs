4345247380c6b9442bd127e8726edb2d3decb087
opehn <67584727+opehn@users.noreply.github.com>
2024-09-30 12:27:34 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240930/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240930/체크판위에서 2/on-the-checkboard-2.cpp
@@ -15,10 +15,11 @@ int main() {
     int sum = 0;
     int max = INT_MIN;
     for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            for(int k = 0; j < n; j++) {
-                cout << "i : " << i << " j : " << j << " k : " << k << endl;
-                sum = arr[i][j] + arr[j][k];
+        for(int j = 0; j < n - 1; j++) {
+            for(int k = i + 1; k < n; k++) {
+                for(int l = 0; l < n - 1; l++) {
+                    sum = (arr[i][j] + arr[i][j + 1]) + (arr[k][l] + arr[k][l + 1]);
+                }
             }
             max = sum > max ? sum : max;
             sum = 0;
