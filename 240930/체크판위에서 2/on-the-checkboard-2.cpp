11da943fa68fbef6b9b3da6984f05f8c27c6b2c1
opehn <67584727+opehn@users.noreply.github.com>
2024-09-30 12:54:38 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240930/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240930/체크판위에서 2/on-the-checkboard-2.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
-#include <climits>
+#include <algorithm>
 using namespace std;
 
+#define MAX_LENGTH 15
 int main() {
-    int n = 5;
-    int arr[5][5] = {{1, 0, 0, 0, 0},
-                 {0, 1, 0, 0, 0},
-                 {0, 1, 1, 0, 1},
-                 {0, 0, 0, 1, 0},
-                 {0, 0, 0, 0, 0}};
+    int n, m;
+    cin >> n >> m;
 
-
-    int cnt = 0;
-    int sum = 0;
-    int max = INT_MIN;
+    char grid[MAX_LENGTH][MAX_LENGTH];
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n - 1; j++) {
-            for(int k = i + 1; k < n; k++) {
-                for(int l = 0; l < n - 1; l++) {
-                    sum = (arr[i][j] + arr[i][j + 1]) + (arr[k][l] + arr[k][l + 1]);
-                    max = sum > max ? sum : max;
-                }
-            }
-
+        for(int j = 0; j < m; j++) {
+            cin >> grid[i][j];
         }
     }
-    cout << max << endl;
+
+    int cnt = 0;
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+            for(int k = i + 1; k < n - 1; k++) {
+                for(int l = j + 1; l < m - 1; l++) {
+                    if(grid[0][0] != grid[i][j] &&
+                        grid[i][j] != grid[k][l] &&
+                        grid[k][l] != grid[n - 1][m - 1]) {
+                            cnt++;
+                    }
+                }
+            }
+        }
+    }
+    cout << cnt;
     return 0;
 }
