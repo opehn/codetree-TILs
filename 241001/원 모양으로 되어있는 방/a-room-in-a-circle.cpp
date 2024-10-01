3bd9d75668646a9eece8d8f803d71d26bb54dcf1
opehn <67584727+opehn@users.noreply.github.com>
2024-10-01 12:17:33 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241001/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+#define MAX_N 1003
+int main() {
+    int n; 
+    cin >> n;
+
+    vector<int> rooms(n);
+    for(int i = 0; i < n; i++){
+        cin >> rooms[i];
+    }
+
+    int total = 0;
+    int start;
+    int dist;
+    int min = INT_MAX;
+    for(int i = 0; i < n; i++){
+        start = i;
+        for(int j = 0; j < n; j++){
+            dist = j - start;
+            dist = dist < 0 ? 5 + dist : dist;
+            total += rooms[j] * dist;
+        }
+        min = total < min ? total : min;
+        total = 0;
+    }
+    cout << min << endl; 
+    return 0;
+}
