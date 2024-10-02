9af5acb3103865113417d061c76f7aa8391c2138
opehn <67584727+opehn@users.noreply.github.com>
2024-10-02 11:09:29 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241002/Carry 피하기 2/escaping-carry-2.cpp
+++ 241002/Carry 피하기 2/escaping-carry-2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <climits>
 #include <vector>
 using namespace std;
 
@@ -30,7 +29,7 @@ int main() {
 
     vector<int> noCarrys;
     int sum;
-    int max = INT_MIN;
+    int max = -1;
     for(int i = 0; i < n - 2; i++){
         for(int j = i + 1; j < n - 1; j++){
             for(int k = j + 1; k < n; k++){
