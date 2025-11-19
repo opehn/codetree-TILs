f9cab56d9f09250ce98ab27bbd9cc07c66410aa2
opehn <67584727+opehn@users.noreply.github.com>
2025-11-20 08:00:41 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251120/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 251120/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -15,7 +15,7 @@ int main(void) {
         if(str[i] == ')') { 
             if (st.empty()) {
                 cout << "No" << endl;
-                return;
+                retunr 0;
             }
             st.pop();
         }
